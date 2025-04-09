d805db1e076ca155ba2edc4ac4a0a5b61c25a911
Amclark9 <amclark9@my.waketech.edu>
2025-04-09 10:30:17 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,16 @@
+// TODO: FETCH DATA FROM DATABASE
+function fetchGradeData() {
+    // RETURN DATABASE DATA
+    console.log("Fetching grade data...")
+}
+
+//TODO: POPULATE THE TABLE WITH DATA
+function populateGradeTable(data) {
+    console.log("Populating gradebook table with data...", data)
+}
+
+//TODO: REMOVE THIS
+//CALL THE STUBS TO SIMULATE WORKFLOW
+const gradeData = fetchGradeData();
+populateGradeTable(gradeData);
+//END REMOVE
